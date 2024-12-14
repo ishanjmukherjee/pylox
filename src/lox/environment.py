926a864b81a35270a3f3d6d9f4955da67877d495
ishanjmukherjee <ishanjmukherjee@gmail.com>
2024-12-14 03:16:41 -0600

    Add explanatory comment and missing import to environment.py
    - Documentation to differentiate Environment's define() and assign() methods
    - Add missing RuntimeError import from interpreter.py
      - Without this, Python's RuntimeError is raised, vomiting a stack trace onto
        the user.

--- src/lox/environment.py
+++ src/lox/environment.py
@@ -8,6 +8,16 @@ class Environment:
         self.values: Dict[str, Any] = {}
         self.enclosing = enclosing
 
+    # define() always creates/updates a variable in the current scope, without
+    # looking at outer scopes. assign() searches through the scope chain to find
+    # an existing variable to update (and throws an error if it can't find one).
+    # Some illustrative code to show how define() and assign() differ:
+    # var answer = 42;    // Uses define() -- creates in current scope
+    # {
+    #   answer = 420;     // Uses assign() -- updates outer x
+    #   var answer = 68;  // Uses define() -- creates NEW x in inner scope
+    #   answer = 70;      // Uses assign() -- updates inner x
+    # }
     def define(self, name: str, value: Any) -> None:
         self.values[name] = value
 
@@ -17,6 +27,8 @@ class Environment:
 
         if self.enclosing is not None:
             return self.enclosing.get(name)
+
+        from lox.interpreter import RuntimeError
 
         raise RuntimeError(name, f"Undefined variable '{name.lexeme}'.")
 
